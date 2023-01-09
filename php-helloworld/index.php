017b833772db48a1a4b54e9733d7bfa0f04e4e9c
Student User <student@workstation.lab.example.com>
2023-01-09 07:26:22 -0500

    added new line

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,5 @@
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 
 print "Abhiroop edited for image update testing in ocp4" ;
+print "tested code change by creating builds from console";
 ?>
